c9b5651870e4618416d6092c790acbd6ae42113d
arevolutioner <andrey.andonov@yahoo.com>
2020-10-15 10:21:58 +0300

    updates

--- main.py
+++ main.py
@@ -138,6 +138,302 @@
 
 # print(longest_name)
 
+# If you don’t expect the lengths of the lists passed to zip to be equal,
+# consider using the zip_longest function from the itertools built-in module instead:
+
+# Things to Remember
+
+# ✦ The zip built-in function can be used to iterate over multiple iterators in parallel.
+# ✦ zip creates a lazy generator that produces tuples, so it can be used on infinitely long inputs.
+# ✦ zip truncates its output silently to the shortest iterator if you supply it with iterators of different lengths.
+# ✦ Use the zip_longest function from the itertools
+# built-in module if you want to use zip on iterators of unequal lengths without truncation.
+
+# 1.5. Avoid else Blocks After for and while Loops
+
+# WRONG
+# a = 4
+# b = 9
+# for i in range ( 2 , min (a, b) + 1 ):
+#     print ( 'Testing' , i)
+#     if a % i == 0 and b % i == 0 :
+#         print ( 'Not coprime' )
+#         break
+# else :
+#     print ( 'Coprime' )
+
+
+#BETTER
+
+# def coprime(a,b):
+#     for i in range(2, min(a,b) + 1):
+#         if a % i ==0 and b % i ==0:
+#             return False
+#     return True
+
+
+# print(coprime(4,9))
+# print(coprime(3,6))
+# assert coprime(4,9)
+# assert not coprime(3,6)
+
+# Things to Remember
+
+# ✦ Python has special syntax that allows else blocks to immediately follow for and while loop interior blocks.
+# ✦ The else block after a loop runs only if the loop body did not encounter a break statement.
+# ✦ Avoid using else blocks after loops because their behavior isn’t intuitive and can be confusing.
+
+ # 1.6. Prevent Repetition with Assignment Expressions
+#
+
+# def make_lemonade(count):
+# ...
+# def out_of_stock():
+# ...
+#
+# fresh_fruit = {
+# 'apple' : 10 ,
+# 'banana' : 8 ,
+# 'lemon' : 5 ,
+# }
+# #
+# # counts = fresh_fruit.get("lemon", 0)
+#
+#
+# if count := fresh_fruit.get( 'lemon' , 0 ):
+#     make_lemonade(count)
+# else :
+#     out_of_stock()
+
+# Things to Remember
+# ✦ Assignment expressions use the walrus operator ( := ) to both assign and evaluate variable names in a single expression, thus reducing repetition.
+# ✦ When an assignment expression is a subexpression of a larger expression, it must be surrounded with parentheses.
+# ✦ Although switch/case statements and do/while loops are not available in Python,
+# their functionality can be emulated much more clearly by using assignment expressions.
+
+# 2. Lists and Dictionaries
+#
+# 2.1 Know How to Slice Sequences
+# #
+# Things to Remember
+# ✦ Avoid being verbose when slicing: Don’t supply 0 for the start index or the length of the sequence for the end index.
+# ✦ Slicing is forgiving of start or end indexes that are out of bounds, which means it’s easy to express slices on the front
+# or back boundaries of a sequence (like a[:20] or a[-20:] ).
+# ✦ Assigning to a list slice replaces that range in the original sequence with what’s referenced even if the lengths are different.
+
+
+# 2.2 Avoid Striding and Slicing in a Single Expression
+
+# x = [ 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' ]
+# x[:: 2 ]
+# ['a', 'c', 'e', 'g'] x[:: 2 ] # ['h', 'f', 'd', 'b'] Here, ::2 means “Select every second item starting at the beginning.”
+# Trickier, ::-2 means “Select every second item starting at the end and moving backward.” What do you think 2::2 means?
+# What about 2::-2 vs. 2:2:-2 vs. 2:2:-2 ? Click here to view code image
+# x[ 2 :: 2 ] #['c', 'e', 'g']
+# x[ -2 :: -2 ] #['g', 'e', 'c', 'a']
+# x[ -2 : 2 : -2 ] #['g', 'e']
+# x[ 2 : 2 : 2 ] # []
+
+# To prevent problems, I suggest you avoid using a stride along with start and end indexes.
+# If you must use a stride, prefer making it a positive value and omit start and end indexes. If you must use a stride
+# with start or end indexes, consider using one assignment for striding and another for slicing: Click here to view code image
+# y = x[:: 2 ] # ['a', 'c', 'e', 'g']
+# z = y[ 1 : 1 ] # ['c', 'e']
+
+# Things to Remember
+#
+# ✦ Specifying start, end, and stride in a slice can be extremely confusing.
+# ✦ Prefer using positive stride values in slices without start or end indexes. Avoid negative stride values if possible.
+# ✦ Avoid using start, end, and stride together in a single slice. If you need all three parameters, consider doing two
+# assignments (one to stride and another to slice) or using islice from the itertools built-in module.
+
+# 2.3 Prefer Catch-All Unpacking Over Slicing
+#
+
+# To better handle this situation, Python also supports catch-all unpacking through a starred expression .
+# This syntax allows one part of the unpacking assignment to receive all values that didn’t match any other part of the unpacking pattern.
+# Here, I use a starred expression to achieve the same result as above without indexing or slicing:
+
+
+# car_ages = [ 0 , 9 , 4 , 8 , 7 , 20 , 19 , 1 , 6 , 15 ]
+#
+# car_ages_descending = sorted(car_ages, reverse = True)
+# #
+# # oldest, second_oldest, *others = car_ages_descending
+# # print (oldest, second_oldest, others)
+#
+# oldest, *others, youngest = car_ages_descending
+# print (oldest, youngest, others)
+
+
+# Things to Remember
+# ✦ Unpacking assignments may use a starred expression to catch all values that
+# weren’t assigned to the other parts of the unpacking pattern into a list.
+# ✦ Starred expressions may appear in any position, and they will always become a
+# list containing the zero or more values they receive.
+# ✦ When dividing a list into non-overlapping pieces, catch-all unpacking is much less error prone than slicing and indexing.
+
+# 2.4. Sort by Complex Criteria Using the key Parameter
+#
+
+# Things to Remember
+# ✦ The sort method of the list type can be used to rearrange a list’s contents by the natural ordering of
+# built-in types like strings, integers, tuples, and so on.
+# ✦ The sort method doesn’t work for objects unless they define a natural ordering using special methods,
+# which is uncommon.
+# ✦ The key parameter of the sort method can be used to supply a helper function that returns the value to use
+# for sorting in place of each item from the list .
+# ✦ Returning a tuple from the key function allows you to combine multiple sorting criteria together.
+# The unary minus operator can be used to reverse individual sort orders for types that allow it.
+# ✦ For types that can’t be negated, you can combine many sorting criteria together by calling the sort method multiple times using
+# different key functions and reverse values, in the order of lowest rank sort call to highest rank sort call.
+
+# 2.5. Be Cautious When Relying on dict Insertion Ordering
+
+# baby_names = {
+# 'cat' : 'kitten' ,
+# 'dog' : 'puppy' ,
+# }
+# print (baby_names)
+
+votes = {
+'otter' : 1281 ,
+'polar bear' : 587 ,
+'fox' : 863 ,
+}
+
+def populate_ranks(votes, ranks):
+    names = list(votes.keys())
+    names.sort(key=votes.get, reverse=True)
+    for i, name in enumerate(names, 1):
+        ranks[name] = i
+
+def get_winner(ranks):
+    return next(iter(ranks))
+
+
+# def get_winner(ranks):
+#     for name, rank in ranks.items():
+#         if rank == 1:
+#             return name
+
+#
+# winner = get_winner(sorted_ranks)
+# print (winner)
+#
+#
+# ranks = {}
+# populate_ranks(votes, ranks)
+# print(ranks)
+# winner = get_winner(ranks)
+# print(winner)
+
+    # Things to Remember
+# ✦ Since Python 3.7, you can rely on the fact that iterating a dict instance’s
+# contents will occur in the same order in which the keys were initially added.
+# ✦ Python makes it easy to define objects that act like dictionaries but that aren’t dict instances.
+# For these types, you can’t assume that insertion ordering will be preserved.
+# ✦ There are three ways to be careful about dictionary-like
+# classes: Write code that doesn’t rely on insertion ordering, explicitly check for the dict type at runtime,
+# or require dict values using type annotations and static analysis.
+
+
+#2.6. Prefer get Over in and KeyError to Handle Missing Dictionary Keys
+
+# counters = {
+# 'pumpernickel' : 2 ,
+# 'sourdough' : 1 ,
+# }
+#
+# key = "wheat"
+# 1.way
+# if key in counters:
+#     count = counters[key]
+# else :
+#     count = 0
+#
+# counters[key] = count + 1
+#2.way
+# try :
+#     count = counters[key]
+# except KeyError:
+#     count = 0
+#
+# counters[key] = count + 1
+#
+#3.way the RIGHT PYTHONIC WAY
+
+# count = counters.get(key,0)
+# counters[key] += count
+# print(counters.items())
+
+# data = {}
+# key = 'foo'
+# value = []
+# data.setdefault(key, value)
+# print ( 'Before:' , data)
+# value.append( 'hello' )
+# print ( 'After: ' , data)
+
+# Things to Remember
+# ✦ There are four common ways to detect and handle missing keys in dictionaries:
+# using in expressions, KeyError exceptions, the get method, and the setdefault method.
+# ✦ The get method is best for dictionaries that contain basic types like counters,
+# and it is preferable along with assignment expressions when creating dictionary values has a high cost or may raise exceptions.
+# ✦ When the setdefault method of dict seems like the best fit for your problem, you should consider using defaultdict instead.
+
+
+# 2.7.Prefer defaultdict Over setdefault to Handle Missing Items in Internal State
+#
+
+# visits = {
+# 'Mexico' : { 'Tulum' , 'Puerto Vallarta' },
+# 'Japan' : { 'Hakone' },
+# }
+#
+# visits.setdefault( 'France',set()).add('Arles')
+#
+# print(visits)
+#
+#
+# if (japan := visits.get( 'Japan' )) is None : # Long
+#     visits[ 'Japan'] = japan = set ()
+# japan.add( 'Kyoto' )
+# print (visits)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
